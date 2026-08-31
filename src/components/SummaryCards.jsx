59267730903f68496c848a51a6d04f5b273a43ef
Stefania <stefpandi12@gmail.com>
2026-08-31 15:10:20 +0300

    feat: create initial static structure for SummarCards component

---
+++ src/components/SummaryCards.jsx
@@ -0,0 +1,13 @@
+export default function SummaryCards() {
+  return (
+    <>
+      {/* Insights Card */}
+      <article className="insight-card">
+        <div className="insight-card__content">
+          <p>Your spending went down 20% last week!</p>
+          <button className="btn-secondary">View stats</button>
+        </div>
+      </article>
+    </>
+  );
+}
